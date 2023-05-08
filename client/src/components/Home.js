103c870cd4f820331444a9ea283ae2871a60c12a
sabeerahamed0511 <sabeerahamed0511@gmail.com>
2023-05-08 14:07:02 +0530

    responsive dsesign for wish card

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -39,24 +39,27 @@ export default function Home() {
                 :
                 error ?
                     <p className="error-text-container">
-                        Please check the URL!<br/>Or<br/>Link may be expired!<br/>&#9785;
+                        Please check the URL!<br />Or<br />Link may be expired!<br />&#9785;
                     </p>
                     :
-                    (
-                        !showForm ?
-                            (currPerson &&
-                                <div id="home-wrapper" >
-                                    <div id="container" className={anime}>
-                                        <p>
-                                            Hello, this is make-a-wish. I'm here to remind you that <em>{currPerson.name}'s birthday</em> is comming soon, so because of our busy schedules, we may forget to wish on time. Nevertheless, you can share your wishes here, and I'll share them with {currPerson.gender === "Male" ? "him" : "her"} at the appropriate time and day <span>&#10083;</span>
-                                        </p>
-                                        <div id="btn-container">
-                                            <button onClick={AddAnime}>Wish here &#9755;</button>
+                    (<div className="lap-container">
+                        {
+                            !showForm ?
+                                (currPerson &&
+                                    <div id="home-wrapper" >
+                                        <div id="container" className={anime}>
+                                            <p>
+                                                Hello, this is make-a-wish. I'm here to remind you that <em>{currPerson.name}'s birthday</em> is comming soon, so because of our busy schedules, we may forget to wish on time. Nevertheless, you can share your wishes here, and I'll share them with {currPerson.gender === "Male" ? "him" : "her"} at the appropriate time and day <span>&#10083;</span>
+                                            </p>
+                                            <div id="btn-container">
+                                                <button onClick={AddAnime}>Wish here &#9755;</button>
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>)
-                            :
-                            <WishForm id={id} currPerson={currPerson} />
+                                    </div>)
+                                :
+                                <WishForm id={id} currPerson={currPerson} />
+                        }
+                    </div>
                     )
         }
     </>
